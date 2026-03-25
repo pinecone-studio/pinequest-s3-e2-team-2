7d850b82eea79c800c1592138cd3244a49728682
Khishigdari <nbhishgee22@gmail.com>
2026-03-25 12:27:21 +0800

    progress bar web student

--- web/app/exam/page.tsx
+++ web/app/exam/page.tsx
@@ -1,8 +1,19 @@
 "use client";
-import { ExamHeader, ExamQA, ExamProgressBar } from "./_components/index";
-import { useExamState } from "./_hooks/use-exam-states";
+
+import { ExamHeader, ExamProgressBar, ExamQA } from "./_components";
+import { ExamProvider, useExamState } from "./_hooks/use-exam-states";
 
 const Exam = () => {
+  return (
+    <ExamProvider>
+      <ExamContent />
+    </ExamProvider>
+  );
+};
+
+export default Exam;
+
+export const ExamContent = () => {
   const { setAnswers, currentId } = useExamState();
 
   const handleAnswerChange = (value: string) => {
@@ -12,7 +23,6 @@ const Exam = () => {
   return (
     <div className="flex flex-col h-screen bg-gray-50 overflow-hidden">
       <ExamHeader />
-
       <div className="flex flex-1 overflow-hidden">
         <div className="flex-1 overflow-y-auto">
           <ExamQA />
@@ -22,5 +32,3 @@ const Exam = () => {
     </div>
   );
 };
-
-export default Exam;
